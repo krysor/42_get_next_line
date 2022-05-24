3cbc76846d559b925c3a52bda82770f03d3383bc
Krystian Kaczorowski <kkaczoro@mi-r4-p5.s19.be>
2022-05-24 17:25:23 +0200

    seems to be working

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kkaczoro <kkaczoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 18:35:38 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/05/24 16:26:28 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/05/24 17:21:10 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*get_next_line(int fd)
 	int			len_next;
 	char		*temp;
 
-	printf("-buffer at beginning f call: %s\n", buffer);
+	//printf("-buffer at beginning f call: %s\n", buffer);
 	if (BUFFER_SIZE <= 0 || fd < 0
 		|| (ft_strlen(buffer) == 0 && read(fd, buffer, BUFFER_SIZE) <= 0))
 		return (NULL);
@@ -46,7 +46,7 @@ char	*get_next_line(int fd)
 			return (NULL);
 		(void)ft_strlcpy(next_line, buffer, ft_strlen(buffer) + 1);
 		(void)ft_memset(buffer, '\0', BUFFER_SIZE);
-		printf("bambam");
+		//printf("bambam");
 		return (next_line);
 	}
 	else
@@ -54,17 +54,20 @@ char	*get_next_line(int fd)
 		temp = malloc(sizeof(char) * (BUFFER_SIZE) + 1);//allocation likely unnecessary
 		if (temp == NULL)
 			return (NULL);
-		printf("entering last case function\n");
+		//printf("entering last case function\n");
 		(void)ft_strlcpy(temp, buffer, ft_strlen(buffer) + 1);
-		printf("--temp last case before read: %s\n", temp);
+		//printf("--temp last case before read: %s\n", temp);
 		len_next = read(fd, buffer, BUFFER_SIZE);//change name of len_next to more intuivitive in the diff function 
-		printf("--temp last case after read: %s\n", temp);
+		//printf("--temp last case after read: %s\n", temp);
 		if (len_next <= 0)
+		{
+			(void)ft_memset(buffer, '\0', BUFFER_SIZE);
 			return (temp);
-		printf("before rec\n");
+		}
+		//printf("before rec\n");
 		new_line = get_next_line(fd);//dont forget to free
-		printf("--temp last case after GNL call: %s\n", temp);
-		printf("new_line after GNL call: %s\n", new_line);
+		//printf("--temp last case after GNL call: %s\n", temp);
+		//printf("new_line after GNL call: %s\n", new_line);
 		if (new_line == NULL)
 		{
 			new_line = malloc(sizeof(char));
@@ -72,7 +75,7 @@ char	*get_next_line(int fd)
 				return (NULL);
 			new_line[0] = '\0';
 		}
-		printf("new_line after NULL check: %s\n", new_line);
+		//printf("new_line after NULL check: %s\n", new_line);
 		next_line = malloc(sizeof(char) * (BUFFER_SIZE + ft_strlen(new_line)) + 1);//if split into different functions: change new_line name to smth more easy to comprehend
 		if (next_line == NULL)
 		{
@@ -80,13 +83,13 @@ char	*get_next_line(int fd)
 			free (new_line);
 			return (NULL);
 		}
-		printf("next_line before 2 strlcpu: %s\n", next_line);
-		printf("--temp before 2 strlcpu: %s\n", temp);
+		//printf("next_line before 2 strlcpu: %s\n", next_line);
+		//printf("--temp before 2 strlcpu: %s\n", temp);
 		(void)ft_strlcpy(next_line, temp, BUFFER_SIZE + 1);
 		(void)ft_strlcpy(next_line + BUFFER_SIZE, new_line, ft_strlen(new_line) + 1);
 		free (temp);
 		free (new_line);
-		printf("next_line after rec: %s\n", next_line);
+		//printf("next_line after rec: %s\n", next_line);
 		return (next_line);
 	}
 }
--- test.c
+++ test.c
@@ -19,9 +19,26 @@
 int	main(void)
 {
 	int	fd = open("/Users/kkaczoro/Desktop/projects/getnextt/get_next_line/text.txt", O_RDONLY);
-	char	*str = get_next_line(fd);
+	//char	*str = get_next_line(fd);
 
-	printf("get_next_line: %s\n", str);
+	printf("get_next_line: %s\n", get_next_line(fd));
+	printf("get_next_line: %s\n", get_next_line(fd));
+	printf("get_next_line: %s\n", get_next_line(fd));
+	printf("get_next_line: %s\n", get_next_line(fd));
+	printf("get_next_line: %s\n", get_next_line(fd));
+	printf("get_next_line: %s\n", get_next_line(fd));
+
+	close(fd);
+	
+	// while (str != NULL)
+	// {
+	// 	printf("get_next_line: %s\n", str);
+	// 	str = get_next_line(fd);
+	// }
+	/*
+	printf("get_next_line: %s\n", get_next_line(fd));
+	printf("get_next_line: %s\n", get_next_line(fd));
+	*/
 	//printf("len_GNL      : %lu\n", strlen(str));
 	// printf("get_next_line: %s\n", get_next_line(fd));
 	// printf("buffer	: %s\n", get_next_line(fd));
